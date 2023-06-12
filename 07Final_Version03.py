bf9ec7fa025a39042d2d6adebab281e27c5fad94
peter123321123321 <98923575+peter123321123321@users.noreply.github.com>
2023-06-12 20:01:40 +1200

    pep8

--- 07Final_Version03.py
+++ 07Final_Version03.py
@@ -23,7 +23,8 @@ def welcome():
 
 def add_monster():
     # Enterbox for user to input MONSTER name
-    monster_name = eg.enterbox("What is the name of your new MONSTER", "MONSTER Name")
+    monster_name = eg.enterbox("What is the name of your new MONSTER",
+                               "MONSTER Name")
     # Check for if the user presses cancel
     if monster_name is None:
         return
@@ -74,8 +75,9 @@ def add_monster():
         else:
             # Asks user what they want to change the stat to
             # + print original stat
-            edit_stat = eg.integerbox(f"The original stat was {cards[monster_name][change]}"
-                                      f"what would you like the new stat to be",
+            edit_stat = eg.integerbox(f"The original stat was "
+                                      f"{cards[monster_name][change]}\n"
+                                      f"what should the new stat to be",
                                       "Stat change",
                                       lowerbound=1, upperbound=25)
             # Check if user pressed cancel
@@ -117,7 +119,7 @@ def remove_monster():
             # it'll go back to the start
             if monster_index >= len(monsters):
                 monster_index = 0
-        # If choice is Delete remove the MONSTER from the list and the dictionary
+        # If choice is Delete remove MONSTER from list and dictionary
         elif choice == "Delete":
             del cards[monster_name]
             monsters.remove(monster_name)
@@ -129,8 +131,8 @@ def remove_monster():
 
 def find_show():
     # Asks user whether they want to find or show MONSTERS
-    choice = eg.buttonbox("Would you like to view a MONSTER or show all MONSTERS",
-                          "Find or Show MONSTERS",
+    choice = eg.buttonbox("Would you like to view a MONSTER or "
+                          "show all MONSTERS", "Find or Show MONSTERS",
                           choices=["Find MONSTER", "Show MONSTERS"])
     if choice == "Show MONSTERS":
         for monster, stat in cards.items():
@@ -144,8 +146,8 @@ def find_show():
             if find == "Exit":
                 break
             # Asks user if the details are correct + prints MONSTER details
-            check = eg.buttonbox(f"Are the details of this MONSTER card correct"
-                                 f"\n{find}, {cards[find]}",
+            check = eg.buttonbox(f"Are the details of this MONSTER card "
+                                 f"correct\n{find}, {cards[find]}",
                                  "Check details", choices=["Yes", "No"])
             # Checks if the user is happy with the MONSTER
             if check == "Yes":
@@ -156,8 +158,8 @@ def find_show():
             # If they want to change the name ask them for the new name
             # then replace the old one with new one
             if change == "Name":
-                new_name = eg.enterbox("What would you like the new MONSTERS name to be",
-                                       "Change MONSTER Name")
+                new_name = eg.enterbox("What would you like the new MONSTERS "
+                                       "name to be", "Change MONSTER Name")
                 # Check if user presses cancel
                 if new_name is None:
                     break
@@ -168,9 +170,11 @@ def find_show():
             else:
                 # Asks user what they want to change the stat to
                 # + print original stat
-                edit_stat = eg.integerbox(f"The original stat was {cards[find][change]} "
-                                          f"what would you like the new stat to be",
-                                          "Stat change", lowerbound=1, upperbound=25)
+                edit_stat = eg.integerbox(f"The original stat was "
+                                          f"{cards[find][change]} "
+                                          f"what would you like "
+                                          f"the new stat to be", "Stat change",
+                                          lowerbound=1, upperbound=25)
                 # Check if user pressed cancel
                 if edit_stat is None:
                     break
@@ -180,24 +184,35 @@ def find_show():
 
 # Main Routine
 # Dictionary storing MONSTER cards
-cards = {"Stoneling": {"Strength": 7, "Speed": 1, "Stealth": 25, "Cunning": 15},
-         "Vexscream": {"Strength": 1, "Speed": 6, "Stealth": 21, "Cunning": 19},
-         "Dawnmirage": {"Strength": 5, "Speed": 15, "Stealth": 18, "Cunning": 22},
-         "Blazegolem": {"Strength": 15, "Speed": 20, "Stealth": 23, "Cunning": 6},
-         "Websnake": {"Strength": 7, "Speed": 15, "Stealth": 10, "Cunning": 5},
-         "Moldvine": {"Strength": 21, "Speed": 18, "Stealth": 14, "Cunning": 5},
-         "Vortexwing": {"Strength": 19, "Speed": 13, "Stealth": 19, "Cunning": 2},
-         "Rotthing": {"Strength": 16, "Speed": 7, "Stealth": 4, "Cunning": 12},
-         "Froststep": {"Strength": 14, "Speed": 14, "Stealth": 17, "Cunning": 4},
-         "Wispghoul": {"Strength": 17, "Speed": 19, "Stealth": 3, "Cunning": 2}}
+cards = {"Stoneling": {"Strength": 7, "Speed": 1,
+                       "Stealth": 25, "Cunning": 15},
+         "Vexscream": {"Strength": 1, "Speed": 6,
+                       "Stealth": 21, "Cunning": 19},
+         "Dawnmirage": {"Strength": 5, "Speed": 15,
+                        "Stealth": 18, "Cunning": 22},
+         "Blazegolem": {"Strength": 15, "Speed": 20,
+                        "Stealth": 23, "Cunning": 6},
+         "Websnake": {"Strength": 7, "Speed": 15,
+                      "Stealth": 10, "Cunning": 5},
+         "Moldvine": {"Strength": 21, "Speed": 18,
+                      "Stealth": 14, "Cunning": 5},
+         "Vortexwing": {"Strength": 19, "Speed": 13,
+                        "Stealth": 19, "Cunning": 2},
+         "Rotthing": {"Strength": 16, "Speed": 7,
+                      "Stealth": 4, "Cunning": 12},
+         "Froststep": {"Strength": 14, "Speed": 14,
+                       "Stealth": 17, "Cunning": 4},
+         "Wispghoul": {"Strength": 17, "Speed": 19,
+                       "Stealth": 3, "Cunning": 2}}
 # List of MONSTER stats
 stats = ["Strength", "Speed", "Stealth", "Cunning"]
 # List of MONSTERS
-monsters = ["Stoneling", "Vexscream", "Dawnmirage", "Blazegolem", "Websnake",
-            "Moldvine", "Vortexwing", "Rotthing",
+monsters = ["Stoneling", "Vexscream", "Dawnmirage", "Blazegolem",
+            "Websnake", "Moldvine", "Vortexwing", "Rotthing",
             "Froststep", "Wispghoul"]
 # Stats + Name for editing
 stat_edit = stats + ["Name"]
 welcome()
 # Prints goodbye message
-eg.msgbox("Thanks for playing with the MONSTER Cards,\nGoodbye, and have fun!", "Goodbye")
+eg.msgbox("Thanks for playing with the MONSTER Cards,\n"
+          "Goodbye, and have fun!", "Goodbye")
